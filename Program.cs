da87279a4440d7091d402e8367030d7631dafdf7
Allan Fantoni <allan.fantoni@outlook.com>
2022-04-28 16:40:19 -0300

    Started to implement write method of Tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using tabuleiro;
+using xadrez_console;
 
-Posicao p = new Posicao(3, 4);
+Tabuleiro tab = new Tabuleiro(8, 8);
 
-Console.WriteLine("Posição: " + p);
-Console.ReadLine();
+Tela.ImprimirTabuleiro(tab);
