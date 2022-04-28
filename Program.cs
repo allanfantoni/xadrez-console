99bfcec961271e6f9cad88e91d38456efe1a7006
Allan Fantoni <allan.fantoni@outlook.com>
2022-04-28 18:11:56 -0300

    Handling errors

--- Program.cs
+++ Program.cs
@@ -2,10 +2,21 @@
 using xadrez;
 using xadrez_console;
 
-Tabuleiro tab = new Tabuleiro(8, 8);
+try
+{
+    Tabuleiro tab = new Tabuleiro(8, 8);
 
-tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+    tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 2));
 
-Tela.ImprimirTabuleiro(tab);
+    Tela.ImprimirTabuleiro(tab);
+}
+catch (TabuleiroException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex) 
+{
+    Console.WriteLine(ex.Message);
+}
