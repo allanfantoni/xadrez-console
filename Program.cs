886325981cffcae4481ca4590d850dd11bcee5c8
Allan Fantoni <allan.fantoni@outlook.com>
2022-04-28 18:34:09 -0300

    Adding PosicaoXadrez class

--- Program.cs
+++ Program.cs
@@ -4,13 +4,19 @@ using xadrez_console;
 
 try
 {
-    Tabuleiro tab = new Tabuleiro(8, 8);
+    //Tabuleiro tab = new Tabuleiro(8, 8);
 
-    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-    tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 2));
+    //tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+    //tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+    //tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 2));
 
-    Tela.ImprimirTabuleiro(tab);
+    //Tela.ImprimirTabuleiro(tab);
+
+    PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+
+    Console.WriteLine(pos);
+    Console.WriteLine(pos.ToPosicao());
+
 }
 catch (TabuleiroException ex)
 {
