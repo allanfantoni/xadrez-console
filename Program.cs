2c21db01fde51b5f593fc67ac2ec1c2520324afc
Allan Fantoni <allan.fantoni@outlook.com>
2022-04-28 16:58:02 -0300

    Started to put Pecas in the Tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 ﻿using tabuleiro;
+using xadrez;
 using xadrez_console;
 
 Tabuleiro tab = new Tabuleiro(8, 8);
+
+tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
 
 Tela.ImprimirTabuleiro(tab);
